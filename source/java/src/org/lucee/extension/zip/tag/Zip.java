37db20e51d023afc18b3b1843c8a0a06e5993dac
Zac Spitzer <zac.spitzer@gmail.com>
2021-07-09 16:36:57 +0200

    update to zip4j 2.9.0

--- source/java/src/org/lucee/extension/zip/tag/Zip.java
+++ source/java/src/org/lucee/extension/zip/tag/Zip.java
@@ -431,7 +431,7 @@ public final class Zip extends BodyTagImpl {
 				query.setAt("name", row, path);
 				query.setAt("size", row, engine.getCastUtil().toDouble(fh.getUncompressedSize())); // TODO do better
 				query.setAt("type", row, fh.isDirectory() ? "Directory" : "File");
-				query.setAt("dateLastModified", row, engine.getCreationUtil().createDateTime(Zip4jUtil.dosToJavaTme(fh.getLastModifiedTime())));
+				query.setAt("dateLastModified", row, engine.getCreationUtil().createDateTime(Zip4jUtil.dosToExtendedEpochTme(fh.getLastModifiedTime())));
 				query.setAt("crc", row, engine.getCastUtil().toDouble(fh.getCrc()));
 				query.setAt("compressedSize", row, engine.getCastUtil().toDouble(fh.getCompressedSize()));
 				query.setAt("comment", row, fh.getFileComment());
@@ -559,7 +559,7 @@ public final class Zip extends BodyTagImpl {
 				if (target instanceof File) {
 					((File) target).setExecutable(true);// TODO was is executable?
 				}
-				target.setLastModified(Zip4jUtil.dosToJavaTme(fh.getLastModifiedTime()));
+				target.setLastModified(Zip4jUtil.dosToExtendedEpochTme(fh.getLastModifiedTime()));
 			}
 		}
 		finally {
@@ -615,7 +615,7 @@ public final class Zip extends BodyTagImpl {
 							// causes problems
 						}
 						else {
-							add(out, in.getInputStream(fh), fh.getFileName(), Zip4jUtil.dosToJavaTme(fh.getLastModifiedTime()), true, toZipParameters(fh));
+							add(out, in.getInputStream(fh), fh.getFileName(), Zip4jUtil.dosToExtendedEpochTme(fh.getLastModifiedTime()), true, toZipParameters(fh));
 						}
 					}
 				}
